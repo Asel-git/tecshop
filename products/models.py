951f6f3ed2afcde490813c7bbc9444be70e241e7
Asel Mambetalieva <asel.mambetalieva.kk@gmail.com>
2022-03-07 21:45:59 +0300

    add user fk in product model

--- products/models.py
+++ products/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Product(models.Model):
+	user = models.ForeignKey(
+		to=User, on_delete=models.SET_NULL,
+		null=True, blank=True
+	)
 	name = models.CharField(max_length=70, null=True, blank=True)
 	description = models.TextField(null=True, blank=True)
 	is_active = models.BooleanField(default=True)
